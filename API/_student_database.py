0004d64e44a021b096893170bd5b067b5d00713b
Grace E Kopp <gkopp@student02.cse.nd.edu>
2017-11-26 20:32:54 -0500

    add init function and load classes

---
+++ API/_student_database.py
@@ -0,0 +1,27 @@
+class _student_database:
+
+    # student database class constructor. It initializes three data
+    # members to empty dictionaries. One containing student information,
+    # one containing class information, and one containing image information.
+    def __init__(self):
+        self.students = {}
+        self.classes = {}
+        self.images = {}
+
+    # Opens a file with the name given as a parameter and loads the class
+    # data from that file into a dictionary with class ID as a key
+    def load_classes(self, file_name):
+        self.classes.clear()
+        for line in open(file_name):
+            class_info = line.strip().split(",")
+            cid = class_info[0]
+            students = []
+            for sid in range(3, len(class_info)):
+                students.append(class_info[sid])
+            class_info = class_info[1:3]
+            class_info.append(students)
+            self.classes[cid] = class_info
+
+if __name__ == '__main__':
+    sdb = _student_database()
+    sdb.load_classes("../data/classes.csv")

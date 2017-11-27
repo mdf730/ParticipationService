46741033926c68d0112fb4336bd0671a64381cf3
Grace E Kopp <gkopp@student04.cse.nd.edu>
2017-11-26 20:57:18 -0500

    implement classes helper functions

--- API/_student_database.py
+++ API/_student_database.py
@@ -14,11 +14,12 @@ class _student_database:
         self.classes.clear()
         for line in open(file_name):
             class_info = line.strip().split(",")
-            cid = class_info[0]
+            cid = int(class_info[0])
             students = []
-            for sid in range(3, len(class_info)):
-                students.append(class_info[sid])
-            class_info = class_info[1:3]
+            for sid in range(4, len(class_info)):
+                students.append(int(class_info[sid]))
+            class_info = class_info[1:4]
+            class_info[2] = int(class_info[2])
             class_info.append(students)
             self.classes[cid] = class_info
 
@@ -34,6 +35,37 @@ class _student_database:
     def reset_images(self):
         self.images.clear()
 
+    # Returns a list of student ID's representing the ID's of the students
+    # in that class
+    def get_students(self, cid):
+        cid = int(cid)
+        return self.classes[cid][3]
+
+    # Return the name of the professor teaching the class indicated by the 
+    # given class ID.
+    def get_professor(self, cid):
+        cid = int(cid)
+        return self.classes[cid][1]
+
+    # Return the name of the class indicated by the given class ID
+    def get_class(self, cid):
+        cid = int(cid)
+        return self.classes[cid][0]
+       
+    # Return the participation point goal of the class indicated by the given
+    # class ID
+    def get_goal(self, cid):
+        cid = int(cid)
+        return self.classes[cid][2]
+
+    def set_goal(self, cid, point_num):
+        cid = int(cid)
+        point_num = int(point_num)
+        self.classes[cid][2] = point_num
+
 if __name__ == '__main__':
     sdb = _student_database()
     sdb.load_classes("../data/classes.csv")
+    print(sdb.get_goal(2))
+    sdb.set_goal(2, 100)
+    print(sdb.get_goal(2))

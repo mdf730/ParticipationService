a5251961617296d08efa6d7b182e5cd76fe16681
Matthew Fabian <mfabian1@nd.edu>
2017-11-30 13:45:45 -0500

    added more methods to server

--- server/main.py
+++ server/main.py
@@ -27,6 +27,31 @@ class Students(object):
 			output['name'] = student
 		return json.dumps(output)
 
+	#GET for /students/{studentID}/{classID}
+	def GET_POINTS(self, sid=None, cid=None):
+		output = {'result':'success'}
+		points = self.sdb.get_points(sid,cid)
+		if points is None:
+			output['result'] = 'error'
+			output['message'] = 'key not found'
+		else:
+			output['id'] = cid
+			output['points'] = str(points)
+		return json.dumps(output)
+
+	#PUT for /students/{studentID}/{classID}
+	def PUT_POINTS(self,sid=None,cid=None):
+		output = {'result':'success'}
+		data = json.loads(cherrypy.request.body.read())
+		try:
+			info = data["points"]
+			self.sdb.set_points(sid,cid,info)
+		except Exception as ex:
+			output['result'] = str(ex)
+		return json.dumps(output)
+
+
+
 ##################################
 # CLASS HANDLER
 ##################################
@@ -39,7 +64,18 @@ class Classes(object):
 		self.sdb.load_students('/home/mfabian1/students.csv')
 		self.sdb.load_images('/home/mfabian1/students.csv')
 
-
+	#GET for /class/:id
+	def GET_ALL(self, id=None):
+		output = {'result':'success'}
+		output['students'] = []
+		for sid in self.sdb.get_students(id):
+			student = self.sdb.get_student(sid)
+			info = {}
+			info['id'] = int(sid)
+			info['name'] = student
+			if student is not None:
+				output['students'].append(info)
+		return json.dumps(output)
 
 ##################################
 # IMAGE HANDLER
@@ -52,6 +88,18 @@ class Images(object):
 		self.sdb.load_classes('/home/mfabian1/classes.csv')
 		self.sdb.load_students('/home/mfabian1/students.csv')
 		self.sdb.load_images('/home/mfabian1/students.csv')
+	
+	#GET for /images/{studentID}
+	def GET(self, id=None):
+		output = {'result':'success'}
+		url = self.sdb.get_image(id)
+		if url is None:
+			output['result'] = 'error'
+			output['message'] = 'key not found'
+		else:
+			output['id'] = id
+			output['url'] = url
+		return json.dumps(output)
 
 ##################################
 # FUNCTIONS
@@ -67,15 +115,22 @@ def start_service():
 
 	#GET:
 	dispatcher.connect('student_get', '/student/:id',controller=students,action = 'GET',conditions=dict(method=['GET']))
-
+	dispatcher.connect('points_get', '/student/:sid/:cid',controller=students,action = 'GET_POINTS',conditions=dict(method=['GET']))
+	#PUT
+	dispatcher.connect('points_put','/student/:sid/:cid',controller=students,action = 'PUT_POINTS',conditions=dict(method=['PUT']))
 
 	# Classes
+	dispatcher.connect('class_get', '/class/:id',controller=classes,action = 'GET_ALL',conditions=dict(method=['GET']))
 
 	# Images
 
+	#GET:
+	dispatcher.connect('image_get', '/image/:id',controller=images,action = 'GET',conditions=dict(method=['GET']))
+
+
 	# Configuration
 	conf = {'global': {'server.socket_host': 'student04.cse.nd.edu', 'server.socket_port': 51029},'/' : {'request.dispatch':dispatcher}}
-	
+
 	#Update configuration and start the server
 	cherrypy.config.update(conf)
 	app = cherrypy.tree.mount(None, config=conf)
